8eb8725d83bfcd7ef85ca6aab69b0feb29283e16
Zac Pentecost <zac.pentecostfs@gmail.com>
2022-10-22 15:36:30 +1000

    Initialize snake game script

---
+++ snake_game.py
@@ -0,0 +1,25 @@
+"""
+Coding a snake game in python for fun.
+
+Zac Pentecost - 22nd October 2022
+"""
+
+import pygame 
+import random 
+
+#----------------------- Define Classes for Game 
+
+
+
+
+if __name__ == '__main__':
+    
+    game = SnakeGame()
+
+    #Game Loop 
+    while True: 
+        game.play_step()
+
+        #Break if game over 
+
+    pygame.quit()
